abccafd2839d4b0f5fc15ab271ae9389dce4a949
Modzel <modzel.tibia@gmail.com>
2022-03-21 20:06:59 +0100

    Add abstract cipher

---
+++ Zadanie_3/Cipher.cs
@@ -0,0 +1,8 @@
+﻿namespace Zadanie_3
+{
+    public abstract class Cipher
+    {
+        public abstract string Encode(string text, string keyString);
+        public abstract string Decode(string text, string keyString);
+    }
+}
